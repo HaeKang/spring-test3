cfb01356809ce9eb4c2b8116ebb62157fff9daa9
haekang <ydjm1027@naver.com>
2023-08-11 08:13:14 +0900

    20230811
    httpservlet 세션

--- login/src/main/java/hello/login/web/HomeController.java
+++ login/src/main/java/hello/login/web/HomeController.java
@@ -2,12 +2,17 @@ package hello.login.web;
 
 import hello.login.domain.member.Member;
 import hello.login.domain.member.MemberRepository;
+import hello.login.web.session.SessionManager;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 @Slf4j
 @Controller
@@ -15,12 +20,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HomeController {
 
     private final MemberRepository memberRepository;
+
+    private final SessionManager sessionManager;
+
 //    @GetMapping("/")
     public String home() {
         return "home";
     }
 
-    @GetMapping("/")
+//    @GetMapping("/")
     public String homeLogin(@CookieValue(name="memberId", required = false) Long memberId, Model model) {
 
         Member loginMember = memberRepository.findById(memberId);
@@ -32,4 +40,62 @@ public class HomeController {
         model.addAttribute("memeber", loginMember);
         return "home";
     }
+
+    // 세션 로그인
+//    @GetMapping("/")
+    public String homeLoginV2(HttpServletRequest request, Model model) {
+
+        // 세션 관리자에 저장된 회원 정보 조회
+        Member member = (Member) sessionManager.getSession(request);
+
+        if (member == null){
+            return "home";
+        }
+
+        model.addAttribute("memeber", member);
+        return "home";
+    }
+
+//    @GetMapping("/")
+    public String homeLoginV3(HttpServletRequest request, Model model) {
+
+        // 세션 관리자에 저장된 회원 정보 조회
+//        Member member = (Member) sessionManager.getSession(request);
+
+        HttpSession session = request.getSession(false);
+        if (session == null){
+            return "home";
+        }
+
+        Member loginMember = (Member) session.getAttribute(SessionConst.LOGIN_MEMEBER);
+
+        if (loginMember == null){
+            return "home";
+        }
+
+        model.addAttribute("memeber", loginMember);
+        return "home";
+    }
+
+    @GetMapping("/")
+    public String homeLoginV3Spring(@SessionAttribute(name=SessionConst.LOGIN_MEMEBER, required = false) Member loginMember, HttpServletRequest request, Model model) {
+
+        // 세션 관리자에 저장된 회원 정보 조회
+//        Member member = (Member) sessionManager.getSession(request);
+//
+//        HttpSession session = request.getSession(false);
+//        if (session == null){
+//            return "home";
+//        }
+//
+//        Member loginMember = (Member) session.getAttribute(SessionConst.LOGIN_MEMEBER);
+
+        if (loginMember == null){
+            return "home";
+        }
+
+        model.addAttribute("memeber", loginMember);
+        return "home";
+    }
+
 }
--- login/src/main/java/hello/login/web/login/LoginController.java
+++ login/src/main/java/hello/login/web/login/LoginController.java
@@ -2,6 +2,8 @@ package hello.login.web.login;
 
 import hello.login.domain.login.LoginService;
 import hello.login.domain.member.Member;
+import hello.login.web.SessionConst;
+import hello.login.web.session.SessionManager;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
@@ -11,7 +13,9 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
 
 @Slf4j
@@ -21,12 +25,14 @@ public class LoginController {
 
     private final LoginService loginService;
 
+    private final SessionManager sessionManager;
+
     @GetMapping("/login")
     public String loginForm(@ModelAttribute("loginForm") LoginForm loginForm){
         return "login/loginForm";
     }
 
-    @PostMapping("/login")
+//    @PostMapping("/login")
     public String login(@Valid @ModelAttribute LoginForm loginForm, BindingResult bindingResult, HttpServletResponse response){
         if (bindingResult.hasErrors()){
             return "login/loginForm";
@@ -47,7 +53,7 @@ public class LoginController {
     }
 
 
-    @PostMapping("/logout")
+//    @PostMapping("/logout")
     public String logout(HttpServletResponse response){
         expireCookie(response, "memberId");
         return "redirect:/";
@@ -58,4 +64,77 @@ public class LoginController {
         cookie.setMaxAge(0);
         response.addCookie(cookie);
     }
+
+
+    // 세션 로그인
+//    @PostMapping("/login")
+    public String loginV2(@Valid @ModelAttribute LoginForm loginForm, BindingResult bindingResult, HttpServletResponse response){
+        if (bindingResult.hasErrors()){
+            return "login/loginForm";
+        }
+
+        Member loginMember = loginService.login(loginForm.getLoginId(), loginForm.getPassword());
+        if(loginMember == null){
+            bindingResult.reject("loginFail", "아이디 혹은 비밀번호가 맞지 않습니다.");
+            return "login/loginForm";
+        }
+
+
+        // 로그인 성공 처리
+
+        // 세션 관리자를 통해 세션을 생성하고 관리
+        sessionManager.createSession(loginMember, response);
+
+        return "redirect:/";
+    }
+
+    // 세션 로그아웃
+//    @PostMapping("/logout")
+    public String logoutV2(HttpServletRequest request, HttpServletResponse response){
+//        expireCookie(response, "memberId");
+
+        sessionManager.expire(request);
+
+        return "redirect:/";
+    }
+
+
+    @PostMapping("/login")
+    public String loginV3(@Valid @ModelAttribute LoginForm loginForm, BindingResult bindingResult, HttpServletRequest request, HttpServletResponse response){
+        if (bindingResult.hasErrors()){
+            return "login/loginForm";
+        }
+
+        Member loginMember = loginService.login(loginForm.getLoginId(), loginForm.getPassword());
+        if(loginMember == null){
+            bindingResult.reject("loginFail", "아이디 혹은 비밀번호가 맞지 않습니다.");
+            return "login/loginForm";
+        }
+
+
+        // 로그인 성공 처리
+
+        // 세션 관리자를 통해 세션을 생성하고 관리
+//        sessionManager.createSession(loginMember, response);
+
+        HttpSession session = request.getSession();
+        session.setAttribute(SessionConst.LOGIN_MEMEBER, loginMember);
+
+        return "redirect:/";
+    }
+
+    @PostMapping("/logout")
+    public String logoutV3(HttpServletRequest request, HttpServletResponse response){
+//        expireCookie(response, "memberId");
+
+//        sessionManager.expire(request);
+
+        HttpSession session = request.getSession(false);
+        if(session != null){
+            session.invalidate();
+        }
+
+        return "redirect:/";
+    }
+
 }
